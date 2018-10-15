af907de35703b27286097f9151aa2280e3130167
Dreamphpr <1095421013@qq.com>
2018-10-15 14:47:36 +0800

    微信支付

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+        Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     });
     // 结束
 });
